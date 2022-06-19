da84575a9676a2e2a8fbafa9dd5fd17afa31baf9
Eduardo Bortoluzzi <eduborto@amazon.com>
2022-06-18 22:45:36 -0300

    add cdk_nag

--- parallel_download/parallel_lambda.py
+++ parallel_download/parallel_lambda.py
@@ -7,23 +7,23 @@ from aws_cdk import (
     Duration,
     aws_lambda as lambda_,
 )
-
 from constructs import Construct
+from cdk_nag import NagSuppressions, NagPackSuppression
 
 
 class Function(Construct):
     def __init__(self, scope: Construct, construct_id: str, *,
-      bucket_name: str,
-      object_key: str,
-      max_workers: int = 1000,
-      runtime: lambda_.Runtime = lambda_.Runtime.PYTHON_3_9,
-      memory_size: Union[int, float, None] = 2304,
-      architecture: lambda_.Architecture = lambda_.Architecture.ARM_64,
-      timeout: Duration = Duration.seconds(300),
-      **kwargs):
+                 bucket_name: str,
+                 max_workers: int = 1000,
+                 runtime: lambda_.Runtime = lambda_.Runtime.PYTHON_3_9,
+                 memory_size: Union[int, float, None] = 2304,
+                 architecture: lambda_.Architecture = lambda_.Architecture.ARM_64,
+                 timeout: Duration = Duration.seconds(300),
+                 **kwargs):
         super().__init__(scope, construct_id)
 
-        self.lambda_function = lambda_.Function(self, construct_id,
+        self.lambda_function = lambda_.Function(
+          self, construct_id,
           runtime=runtime,
           memory_size=memory_size,
           architecture=architecture,
@@ -32,8 +32,20 @@ class Function(Construct):
           code=lambda_.Code.from_asset("resources"),
           environment={
               "BUCKET": bucket_name,
-              "OBJECT_KEY": object_key,
               "MAX_WORKERS": str(max_workers),
           },
           **kwargs
+        )
+
+        NagSuppressions.add_resource_suppressions(
+          self.lambda_function.role,
+          [
+            NagPackSuppression(
+              id="AwsSolutions-IAM4",
+              reason=("Using the AWSLambdaBasicExecutionRole managed policy to be able to send logs"
+                      "to CloudWatch"),
+              applies_to=[("Policy::arn:<AWS::Partition>:iam::aws:policy/"
+                           "service-role/AWSLambdaBasicExecutionRole")]
+            )
+          ]
         )
